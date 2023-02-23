a5879b8a47fd8ba4d37e5c26691be0fd0e156272
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2023-02-22 22:16:36 -0500

    connecting mongodb

--- index.js
+++ index.js
@@ -1,12 +1,8 @@
-import express from "express"
-import cors from "cors"
-import reviews from "./api/reviews.route.js"
+import app from "./server.js"
+import mongodb from "mongodb"
+import ReviewsDAO from "./dao/reviewsDAO.js"
 
-const app = express()
-
-app.use(cors())
-app.use(express.json())
-
-app.use("/api/v1/reviews", reviews)
-app.use("*", (req, res) => 
-res.status(404).json({error: "not found"}))
+const MongoClient = mongodb.MongoClient
+const mongo_username = process.env['MONGO_USERNAME']
+const mongo_password = process.env['MONGO_PASSWORD']
+const uri = `mongodb+srv://${mongo_username}:${mongo_password}@reviewdb.r3tjfhe.mongodb.net/?retryWrites=true&w=majority`
