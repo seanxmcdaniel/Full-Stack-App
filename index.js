72a90e2d2cc6d51e5349c5c1cd2350e8e51fe9ad
seanxmcdaniel <102200863+seanxmcdaniel@users.noreply.github.com>
2023-02-23 14:03:43 -0500

    Connecting to MongoDB Database

--- index.js
+++ index.js
@@ -1,8 +1,26 @@
 import app from "./server.js"
 import mongodb from "mongodb"
-import ReviewsDAO from "./dao/reviewsDAO.js"
+// import ReviewsDAO from "./dao/reviewsDAO.js"
 
 const MongoClient = mongodb.MongoClient
-const mongo_username = process.env['MONGO_USERNAME']
-const mongo_password = process.env['MONGO_PASSWORD']
-const uri = `mongodb+srv://${mongo_username}:${mongo_password}@reviewdb.r3tjfhe.mongodb.net/?retryWrites=true&w=majority`
+const uri = `mongodb+srv://sean:moviedb@reviewdb.r3tjfhe.mongodb.net/?retryWrites=true&w=majority`
+
+const port = process.env.PORT || 8000
+
+MongoClient.connect(
+  uri,
+  {
+    maxPoolSize: 50,
+    wtimeoutMS: 2500,
+    useNewUrlParser: true
+  })
+  .catch(err => {
+    console.error(err.stack)
+    process.exit(1)
+  })
+  .then(async client => {
+    // await ReviewsDAO.injectDB(client)
+    app.listen(port, () => {
+      console.log(`MongoDB connected. Listening on port ${port}`)
+    })
+  })
